c1c1d908d774058be12afb5a5359c04da2e45b56
Ethaniel Billon <ebillon@z2r2p1.42lyon.fr>
2025-10-02 10:01:42 +0200

    ´tests´

--- install.sh
+++ install.sh
@@ -11,20 +11,26 @@ TIRUM_RADARR_MOVIES_FOLDER="${PWD}/managers/radarr/movies/"
 
 TIRUM_PROWLARR_CONFIG_FOLDER="${PWD}/managers/prowlarr/config/"
 
-mkdir -p ${TIRUM_TORRENT_CONFIG_FOLDER} ${TIRUM_TORRENT_DOWNLOAD_FOLDER} \
+function createFolder {
+    mkdir -p ${TIRUM_TORRENT_CONFIG_FOLDER} ${TIRUM_TORRENT_DOWNLOAD_FOLDER} \
 ${TIRUM_RADARR_CONFIG_FOLDER} ${TIRUM_RADARR_MOVIES_FOLDER} \
 ${TIRUM_PROWLARR_CONFIG_FOLDER}
+}
 
 ## simlinks
 TIRUM_LIBRARY_FOLDER="${PWD}/library/"
 TIRUM_MOVIES_FOLDER="${TIRUM_LIBRARY_FOLDER}movies"
 
-mkdir -p ${TIRUM_LIBRARY_FOLDER}
+function createSimlinks {
+    mkdir -p ${TIRUM_LIBRARY_FOLDER}
 
-ln -snf ${TIRUM_RADARR_MOVIES_FOLDER} ${TIRUM_MOVIES_FOLDER}
+    ln -snf ${TIRUM_RADARR_MOVIES_FOLDER} ${TIRUM_MOVIES_FOLDER}
+}
 
 ## create env
-cat > .env <<EOF
+
+function createEnv {
+    cat > .env <<EOF
 TIRUM_TORRENT_CONFIG_FOLDER=${TIRUM_TORRENT_CONFIG_FOLDER}
 TIRUM_TORRENT_DOWNLOAD_FOLDER=${TIRUM_TORRENT_DOWNLOAD_FOLDER}
 TIRUM_RADARR_CONFIG_FOLDER=${TIRUM_RADARR_CONFIG_FOLDER}
@@ -33,15 +39,37 @@ TIRUM_PROWLARR_CONFIG_FOLDER=${TIRUM_PROWLARR_CONFIG_FOLDER}
 TIRUM_LIBRARY_FOLDER=${TIRUM_LIBRARY_FOLDER}
 TIRUM_MOVIES_FOLDER=${TIRUM_MOVIES_FOLDER}
 EOF
-
-docker compose up --build -d
-
-## scripting functions
-function installUv {
-    
 }
 
+## scripting functions
+CACHE_FOLDER="${PWD}/.cache/"
+UV_LINK="https://github.com/astral-sh/uv/releases/download/0.8.22/uv-aarch64-unknown-linux-gnu.tar.gz"
+UV="${CACHE_FOLDER}uv"
+
+function _ensureCache {
+    mkdir -p ${CACHE_FOLDER}
+}
+
+function cleanCache {
+    rm -rf ${CACHE_FOLDER}
+}
+
+function installUv {
+    _ensureCache
+    wget --directory-prefix=${CACHE_FOLDER} https://github.com/astral-sh/uv/releases/download/0.8.22/uv-aarch64-unknown-linux-gnu.tar.gz
+}
+
+function runner {
+    createFolder
+    createSimlinks
+    createEnv
+    # installUv
+}
+
+runner
+
 ## configure qbittorrent
-sleep 2
-PASSWORD=$(docker logs qbittorrent 2>&1 | grep "temporary password" | awk -F': ' '{print $2}')
-uv run --with requests ./scripts/qbittorrent.py "${PASSWORD}"
+# sleep 2
+# PASSWORD=$(docker logs qbittorrent 2>&1 | grep "temporary password" | awk -F': ' '{print $2}')
+# uv run --with requests ./scripts/qbittorrent.py "${PASSWORD}"
+
